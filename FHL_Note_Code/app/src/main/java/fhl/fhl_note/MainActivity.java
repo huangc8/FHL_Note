53da7a6fcf6190edd06a40b41b6f69472b14af4b
cnhuang <cnhuang851@gmail.com>
2016-08-19 15:38:15 -0400

    direct link code implemented

--- FHL_Note_Code/app/src/main/java/fhl/fhl_note/MainActivity.java
+++ FHL_Note_Code/app/src/main/java/fhl/fhl_note/MainActivity.java
@@ -1,7 +1,8 @@
 package fhl.fhl_note;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+    }
+
+    // initialize
+    protected void initialize(){
+        WebView webview = new WebView(this);
+        setContentView(webview);
+
+        webview.loadUrl("http://a2z.fhl.net/bible/");
     }
 }
